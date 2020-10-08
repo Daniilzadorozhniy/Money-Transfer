d406bfd3055f00133cc3f009a0cf1a9288191af2
Daniil Zadorozhniy <daniil.zadorozhniy1991@gmail.com>
2020-10-08 15:33:32 +0700

    Initifl commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transferamount = 500_000_000;
+        int totalSum = balance + transferamount;
+        System.out.println(totalSum);
+    }
+
+}
